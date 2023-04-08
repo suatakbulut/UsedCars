d430338216d4906826f2eb5de8cf37fa822fb341
suatakbulut <akbulut.suat@gmail.com>
2023-04-08 16:47:40 -0400

    updated scraping reviews and complaints

--- ScrapeTools.py
+++ ScrapeTools.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+
 def extract_msrp(text):
     """
     Finds subtexts that match the pattern, sorts it, and returns the middle value. 
@@ -12,12 +13,13 @@ def extract_msrp(text):
         result = pattern.findall(text)
         result = sorted(result)
         msrp = result[len(result)//2]
-        MSRP = msrp.replace(",","")
-        return MSRP[1:] 
+        MSRP = msrp.replace(",", "")
+        return MSRP[1:]
     except:
         return None
 
-def scrape_GoogleSearch(CarName, mobile=True): 
+
+def scrape_GoogleSearch(CarName, mobile=True):
     """ 
     Takes a string formatted as maker-model-year.
     If google.com/search has the MSRP value in the first main div block called g.mnr-c.g-blk
@@ -27,26 +29,27 @@ def scrape_GoogleSearch(CarName, mobile=True):
         car = CarName.replace("-", "+")
         car = car.replace(" ", "+")
         query = "+".join([car, "msrp"])
-        url = "https://google.com/search?q=<{}>".format(query) 
+        url = "https://google.com/search?q=<{}>".format(query)
         # desktop user-agent
         USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
         # mobile user-agent
         MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
-        
+
         if mobile:
-            headers = {"user-agent" : MOBILE_USER_AGENT}
+            headers = {"user-agent": MOBILE_USER_AGENT}
         else:
-            headers = {"user-agent" : USER_AGENT}
-            
-        resp = requests.get(url, headers=headers)   
+            headers = {"user-agent": USER_AGENT}
+
+        resp = requests.get(url, headers=headers)
         soup = BeautifulSoup(resp.content, 'lxml')
-        text = str(soup.find("div", attrs={"class":"xpdopen"})) 
-        carMSRP = extract_msrp(text) 
+        text = str(soup.find("div", attrs={"class": "xpdopen"}))
+        carMSRP = extract_msrp(text)
         if carMSRP:
             line = ",".join([CarName, carMSRP]) + "\n"
             return line
     except:
         pass
+
 
 def scrape_NewCarsTestDrive(CarName):
     """ 
@@ -58,26 +61,82 @@ def scrape_NewCarsTestDrive(CarName):
         url = "https://www.newcartestdrive.com/reviews/{}/".format(CarName)
         response = requests.get(url)
         urlSoup = BeautifulSoup(response.content, "lxml")
-        
-        start = urlSoup.find(lambda tag: tag.name == 'h2' and 'model line' in tag.text.lower())
+
+        start = urlSoup.find(lambda tag: tag.name ==
+                             'h2' and 'model line' in tag.text.lower())
         end = start.findNext("h2")
         item = start.nextSibling
         text = ''
-        while item != end: 
-            text += str(item) 
+        while item != end:
+            text += str(item)
             item = item.nextSibling
-        
+
         carMSRP = extract_msrp(text)
-        line = ",".join([CarName, carMSRP]) + "\n" 
+        line = ",".join([CarName, carMSRP]) + "\n"
         return line
     except:
         pass
 
-if __name__ == '__main__': 
+
+def scrape_reviews(car_name):
+    # format should be 'make-model-year', e.g. 'honda-accord-2014'
+    url = f'https://www.cars.com/research/{car_name}/consumer-reviews/'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(response.content, "lxml")
+    # focus on the summary div
+    summary = soup.find("div", attrs={"class": "summary-container"})
+
+    # overall star score
+    score = summary.find("span", attrs={"class": "sds-rating__count"}).text
+
+    # number reviews
+    rev_count = summary.find(
+        "a", attrs={"class": "sds-rating__link sds-button-link"}).text
+    num_reviews = "".join([ch for ch in rev_count if ch.isdigit()])
+
+    # Recommendation rate, e.g. '65% of drivers recommend this car'
+    recommendation_rate = summary.find(text=re.compile('recommend'))
+
+    # Recommendation Breakdown
+    breakdown = summary.find(
+        "ul", attrs={"class": "sds-definition-list review-breakdown--list"})
+    breakdown_map = {}
+    details = [det for det in breakdown.text.split("\n") if det]
+    for i in range(len(details)//2):
+        breakdown_map[details[2*i]] = details[2*i+1]
+
+    reviews = {
+        "score": score,
+        "num_reviews": num_reviews,
+        "recommendation_rate": recommendation_rate,
+        "breakdown_map": breakdown_map
+    }
+    return reviews
+
+
+def scrape_complaints(manufacturer, model, year, numComplaints=6):
+    manufacturer = manufacturer.title() 
+    model = model.title() 
+    try:
+        url = f'https://www.carcomplaints.com/{manufacturer}/{model}/{year}/'
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        response = requests.get(url, headers=headers)
+        urlSoup = BeautifulSoup(response.content, "lxml")
+        container = urlSoup.find("div", attrs={"id": "graph"})
+        complaints_list = container.ul.findAll('li')
+        return [complaints_list[ind].strong.text.capitalize() for ind in range(min(len(complaints_list), numComplaints))]
+
+    except:
+        print('Could not retreive complaints. . Error with Car.complaints() method.')
+        return ['No mechanical issues retrieved']
+
+
+if __name__ == '__main__':
     car1 = '2018-toyota-camry'
     car2 = '2012-ford-focus'
     for car in (car1, car2):
         print(scrape_NewCarsTestDrive(car))
         print(scrape_GoogleSearch(car))
-    
-    
--- functions.py
+++ functions.py
@@ -1,237 +1,216 @@
 # test
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup 
-import dill 
+from bs4 import BeautifulSoup
+import dill
 from PIL import Image
-import os 
-import altair as alt 
-from datetime import datetime 
+import os
+import altair as alt
+from datetime import datetime
 from itertools import product
 import chart_studio.tools as tls
 import chart_studio.plotly as py
-import plotly.graph_objects as go 
+import plotly.graph_objects as go
 import numpy as np
+from ScrapeTools import scrape_reviews, scrape_complaints
+
 
 class Model:
-	def __init__(self, model_path='static/model.dill'):
-		self.model = dill.load(open(model_path, 'rb')) 
+    def __init__(self, model_path='static/model.dill'):
+        self.model = dill.load(open(model_path, 'rb'))
+
 
 class Car:
-	def __init__(self, car_data):
-		self.state = car_data['state'][0]
-		self.year = car_data['year'][0]
-		self.manufacturer = car_data['manufacturer'][0]
-		self.model = car_data['model'][0]
-		self.odometer = car_data['odometer'][0]
-		self.transmission = car_data['transmission'][0]
-		self.condition = car_data['condition'][0]
-		self.BaseMSRP = car_data['BaseMSRP'][0]
-		self.df = pd.DataFrame.from_dict(car_data) 
+    def __init__(self, car_data):
+        self.state = car_data['state'][0]
+        self.year = car_data['year'][0]
+        self.manufacturer = car_data['manufacturer'][0]
+        self.model = car_data['model'][0]
+        self.odometer = car_data['odometer'][0]
+        self.transmission = car_data['transmission'][0]
+        self.condition = car_data['condition'][0]
+        self.BaseMSRP = car_data['BaseMSRP'][0]
+        self.df = pd.DataFrame.from_dict(car_data)
 
-	def fullName(self):
-		return '-'.join([str(self.year), self.manufacturer, self.model]) 
+    def fullName(self):
+        return '-'.join([str(self.year), self.manufacturer, self.model])
 
-	def titleName(self):
-		return ' '.join([str(self.year), self.manufacturer.capitalize(), self.model.capitalize()]) 
+    def titleName(self):
+        return ' '.join([str(self.year), self.manufacturer.capitalize(), self.model.capitalize()])
 
-	def complaints(self, numComplaints=6):
-		try:
-			url = f'https://www.carcomplaints.com/{self.manufacturer}/{self.model}/{self.year}/' 
-			headers={'User-Agent': 'Mozilla/5.0'} 
-			response = requests.get(url, headers=headers) 
-			urlSoup = BeautifulSoup(response.content, "lxml")        
-			container = urlSoup.find("div", attrs={"id":"graph"})
-			complaints_list = container.ul.findAll('li')
-			return [complaints_list[ind].strong.text.capitalize() for ind in range( min(len(complaints_list), numComplaints) )]
+    def complaints(self, numComplaints=6):
+        try:
+            complaints = scrape_complaints(self.manufacturer, self.model, self.year, numComplaints=numComplaints)
+            return complaints
 
-		except:
-			print('Could not retreive complaints. . Error with Car.complaints() method.')
-			return ['No mechanical issues retrieved']
-			
-	def price(self, Model):
-		return int(Model.model.predict(self.df)[0])
+        except:
+            print('Could not retreive complaints. . Error with Car.complaints() method.')
+            return ['No mechanical issues retrieved']
 
-	def reviews(self):
-		try:
-			model = self.model.replace('-', '_')
-			model = model.replace(' ', '_')
-			car = '-'.join([self.manufacturer, model, str(self.year)])
-			url = f'https://www.cars.com/research/{car}/consumer-reviews/' 
-			headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}         
-			response = requests.get(url, headers=headers) 
-			urlSoup = BeautifulSoup(response.content, "lxml")        
-			
-			container = urlSoup.find("div", attrs={"class":"review-header-details"})
-			reviews = {} 
+    def price(self, Model):
+        return int(Model.model.predict(self.df)[0])
 
-			overall = container.find('div', attrs={'class' : 'review-overall-stars'} )
-			reviews['overall'] = { 
-				'score': overall.p.text, 
-				'num_reviews': overall.span.text}
+    def reviews(self):
+        try:
+            model = self.model.replace('-', '_')
+            model = model.replace(' ', '_')
+            car_name = '-'.join([self.manufacturer, model, str(self.year)])
+            reviews = scrape_reviews(car_name)
+            return reviews
+        except:
+            print('Could not retreive any reviews. . Error with Car.reviews() method.')
+            reviews = {
+                "score": "Null",
+                "num_reviews": "Null",
+                "recommendation_rate": "Null% of drivers recommend this car",
+                "breakdown_map": {}
+            }
+            return reviews
 
-			recommendation = container.find('div', attrs={'class' : 'rcmd-rate'} )
-			reviews['recommendation_rate'] = recommendation.strong.text
-			
-			details_table = container.find('table', attrs={'class' : 'review-overall-averages'} )
-			details_rows = details_table.findAll('tr')
-			details = {}
-			for details_row in details_rows:
-				score, feature = details_row.text.replace('\n', '').split(' out of 5 stars')
-				details[feature] = score
-			reviews['details'] = details 
+    def pic(self, car_img_dir='static'):
+        try:
+            url = f"https://www.newcartestdrive.com/reviews/{self.fullName()}/"
+            response = requests.get(url)
+            urlSoup = BeautifulSoup(response.content, "html.parser")
+            img_div = urlSoup.find("div", attrs={"id": "thumb-box"})
+            img_url = img_div.img.attrs['src']
+            img = Image.open(requests.get(img_url, stream=True).raw)
+            file_path = os.path.join(car_img_dir, 'car_img.jpg')
+            img.save(file_path)
+            return f"{self.fullName()}'s pic is successfully scraped"
+        except:
+            print('Could not retreive the image. Error with Car.pic() method.')
+            pass
 
-			return reviews 
-			
-		except:
-			print('Could not retreive any reviews. . Error with Car.reviews() method.')
-			reviews = {
-				'details' : [], 
-				'recommendation_rate' : None, 
-				'overall' : {'score': None, 'num_reviews': None} 
-				}
-			return reviews
+    def barChart(self, Model):
+        years = range(self.year-2, min(self.year+3, datetime.now().year+1))
+        conditions = ('fair', 'good', 'excellent', 'like new')
+        df = pd.DataFrame(list(product(years, conditions)),
+                          columns=['year', 'condition'])
+        for col in set(self.df.columns).difference(set(df.columns)):
+            df[col] = self.df[col][0]
+        cols = ['state', 'year', 'manufacturer', 'model',
+                'odometer', 'transmission', 'condition', 'BaseMSRP']
+        df = df[cols]
+        df['price'] = Model.model.predict(df)
+        barChart = alt.Chart(df).mark_bar().encode(
+            x=alt.X('condition:O', sort=[
+                    'fair', 'good', 'excellent', 'like new']),
+            y='price:Q',
+            color=alt.Color('condition:O', sort=[
+                            'fair', 'good', 'excellent', 'like new'], scale=alt.Scale(scheme='purplegreen')),
+            column='year:N').properties(
+            height=240,
+            width=140,
+            padding=10,
+            autosize=alt.AutoSizeParams(
+                type='fit',
+                contains='padding'
+            ))
 
-	def pic(self, car_img_dir='static'):
-		try:
-			url = f"https://www.newcartestdrive.com/reviews/{self.fullName()}/"
-			response = requests.get(url)
-			urlSoup = BeautifulSoup(response.content, "html.parser")
-			img_div = urlSoup.find("div", attrs={"id":"thumb-box"})
-			img_url = img_div.img.attrs['src'] 
-			img = Image.open(requests.get(img_url, stream = True).raw) 
-			file_path = os.path.join(car_img_dir, 'car_img.jpg')
-			img.save(file_path)    
-			return f"{self.fullName()}'s pic is successfully scraped"
-		except:
-			print('Could not retreive the image. Error with Car.pic() method.')
-			pass
+        # return barChart.to_json(indent=None).encode('utf-8')
+        return barChart.to_json(indent=None)
 
-	def barChart(self, Model):	
-		years = range( self.year-2, min(self.year+3, datetime.now().year+1) )
-		conditions = ('fair', 'good', 'excellent', 'like new')
-		df = pd.DataFrame( list(product(years, conditions)) , columns = ['year', 'condition'])
-		for col in set(self.df.columns).difference(set(df.columns)):
-			df[col] = self.df[col][0]
-		cols = ['state', 'year', 'manufacturer', 'model', 'odometer', 'transmission', 'condition', 'BaseMSRP']
-		df = df[cols]
-		df['price'] = Model.model.predict(df) 
-		barChart = alt.Chart(df).mark_bar().encode( 
-			x=alt.X('condition:O', sort=['fair', 'good', 'excellent', 'like new']),
-			y='price:Q',
-			color=alt.Color('condition:O', sort=['fair', 'good', 'excellent', 'like new'], scale=alt.Scale(scheme='purplegreen')),
-			column='year:N').properties(
-				height = 240,
-				width = 140,  
-				padding = 10, 
-				autosize=alt.AutoSizeParams(
-				type = 'fit',
-				contains = 'padding'
-				) )
+    def map(self, Model):
+        username = 'suatakbulut'
+        api_key = 'OmWwvBMIO3rYLry5fn5F'
+        tls.set_credentials_file(username=username, api_key=api_key)
 
-		#return barChart.to_json(indent=None).encode('utf-8') 
-		return barChart.to_json(indent=None) 
+        states = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'dc', 'de', 'fl',
+                        'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me',
+                        'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh',
+                        'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri',
+                        'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy']
+        locations = [state.upper() for state in states]
 
-	def map(self, Model):
-		username = 'suatakbulut'
-		api_key = 'OmWwvBMIO3rYLry5fn5F'
-		tls.set_credentials_file(username=username, api_key=api_key)
+        df = self.df.copy()
+        df = df.loc[df.index.repeat(len(states))].reset_index(drop=True)
+        df['state'] = states
+        preds = Model.model.predict(df)
+        predictions = [int(pred) for pred in preds]
 
-		states = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'dc', 'de', 'fl', 
-				'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me', 
-				'md', 'ma', 'mi', 'mn', 'ms', 'mo', 'mt', 'ne', 'nv', 'nh', 
-				'nj', 'nm', 'ny', 'nc', 'nd', 'oh', 'ok', 'or', 'pa', 'ri', 
-				'sc', 'sd', 'tn', 'tx', 'ut', 'vt', 'va', 'wa', 'wv', 'wi', 'wy']
-		locations = [state.upper() for state in states]
-		
-		df = self.df.copy() 
-		df = df.loc[df.index.repeat(len(states))].reset_index(drop = True) 
-		df['state'] = states 
-		preds = Model.model.predict(df) 
-		predictions = [int(pred) for pred in preds]
+        fig = go.Figure(data=go.Choropleth(
+            locations=locations,  # Spatial coordinates
+            z=predictions,  # Data to be color-coded
+            locationmode='USA-states',  # set of locations match entries in `locations`
+            colorscale='PRGn',
+            colorbar_title="Thousands USD",
+        ))
 
-		fig = go.Figure(data=go.Choropleth(
-			locations=locations, # Spatial coordinates
-			z = predictions, # Data to be color-coded
-			locationmode = 'USA-states', # set of locations match entries in `locations`
-			colorscale = 'PRGn',
-			colorbar_title = "Thousands USD",
-		))
+        fig.update_layout(
+            title_text=f'Used {self.titleName()} in {self.condition.capitalize()} Condition Prices by State',
+            geo_scope='usa',  # limite map scope to USA
+        )
 
-		fig.update_layout(
-			title_text = f'Used {self.titleName()} in {self.condition.capitalize()} Condition Prices by State',
-			geo_scope='usa', # limite map scope to USA
-		)
+        return py.plot(fig, filename='usedCars_map', auto_open=False)
 
-		return py.plot(fig, filename = 'usedCars_map', auto_open=False) 
 
-if __name__ == '__main__': 
-	car_data_1 = {
-		'state' : ['al'], 
-		'year' : [2014], 
-		'manufacturer' : ['hyundai'], 
-		'model' : ['sonata'], 
-		'odometer' : [93600], 
-		'transmission' : ['automatic'], 
-		'condition' : ['excellent'], 
-		'BaseMSRP' : [0]
-		} 
+if __name__ == '__main__':
+    car_data_1 = {
+        'state': ['al'],
+        'year': [2014],
+        'manufacturer': ['hyundai'],
+        'model': ['sonata'],
+        'odometer': [93600],
+        'transmission': ['automatic'],
+        'condition': ['excellent'],
+        'BaseMSRP': [0]
+    }
 
-	car_data_2 = {
-		'state' : ['al'], 
-		'year' : [2016], 
-		'manufacturer' : ['mazda'], 
-		'model' : ['cx-5'], 
-		'odometer' : [54000], 
-		'transmission' : ['automatic'], 
-		'condition' : ['good'], 
-		'BaseMSRP' : [0]
-		} 
+    car_data_2 = {
+        'state': ['al'],
+        'year': [2016],
+        'manufacturer': ['mazda'],
+        'model': ['cx-5'],
+        'odometer': [54000],
+        'transmission': ['automatic'],
+        'condition': ['good'],
+        'BaseMSRP': [0]
+    }
 
-	model = Model(model_path='static/model.dill')
-	car1 = Car(car_data_1)
-	car2 = Car(car_data_2)
+    model = Model(model_path='static/model.dill')
+    car1 = Car(car_data_1)
+    car2 = Car(car_data_2)
 
-	testAll = True   
-    
-	if testAll:
-		print('\nTesting fullName.')
-		print(car1.fullName())
-		print(car2.fullName())
-		print('=================')
+    testAll = True
 
-		print('\nTesting titleName.')
-		print(car1.titleName())
-		print(car2.titleName())
-		print('=================')
+    if testAll:
+        print('\nTesting fullName.')
+        print(car1.fullName())
+        print(car2.fullName())
+        print('=================')
 
-		print('\nTesting complaints.')
-		print(f'{car1.titleName()} : {car1.complaints()}') 
-		print(f'{car2.titleName()} : {car2.complaints()}')
-		print('=================')
+        print('\nTesting titleName.')
+        print(car1.titleName())
+        print(car2.titleName())
+        print('=================')
 
-		print('\nTesting price.')
-		print(f'{car1.titleName()} : {car1.price(model)}') 
-		print(f'{car2.titleName()} : {car2.price(model)}')
-		print('=================')
+        print('\nTesting complaints.')
+        print(f'{car1.titleName()} : {car1.complaints()}')
+        print(f'{car2.titleName()} : {car2.complaints()}')
+        print('=================')
 
-		print('\nTesting reviews.')
-		print(f'{car1.titleName()} : {car1.reviews()}') 
-		print(f'{car2.titleName()} : {car2.reviews()}')
-		print('=================')
+        print('\nTesting price.')
+        print(f'{car1.titleName()} : {car1.price(model)}')
+        print(f'{car2.titleName()} : {car2.price(model)}')
+        print('=================')
 
-		print('\nTesting pic.')
-		print(f'{car1.titleName()} : {car1.pic()}') 
-		print(f'{car2.titleName()} : {car2.pic()}')
-		print('=================')
+        print('\nTesting reviews.')
+        print(f'{car1.titleName()} : {car1.reviews()}')
+        print(f'{car2.titleName()} : {car2.reviews()}')
+        print('=================')
 
-		print('\nTesting barChart.')
-		print(f'{car1.titleName()} : {car1.barChart(model)}') 
-		print(f'{car2.titleName()} : {car2.barChart(model)}')
-		print('=================')	
+        print('\nTesting pic.')
+        print(f'{car1.titleName()} : {car1.pic()}')
+        print(f'{car2.titleName()} : {car2.pic()}')
+        print('=================')
 
-		print('\nTesting map.')
-		print(f'{car1.titleName()} : {car1.map(model)}') 
-		print(f'{car2.titleName()} : {car2.map(model)}')
-		print('=================')	
+        print('\nTesting barChart.')
+        print(f'{car1.titleName()} : {car1.barChart(model)}')
+        print(f'{car2.titleName()} : {car2.barChart(model)}')
+        print('=================')
 
+        print('\nTesting map.')
+        print(f'{car1.titleName()} : {car1.map(model)}')
+        print(f'{car2.titleName()} : {car2.map(model)}')
+        print('=================')
